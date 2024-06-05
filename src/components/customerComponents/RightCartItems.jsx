58035054c40e34aa32c553a8b78a6b358a6364a7
lalitjakhar <lalit25jakhar@gmail.com>
2024-06-05 19:26:26 +0530

    Homepage CartItem ui

--- src/components/customerComponents/RightCartItems.jsx
+++ src/components/customerComponents/RightCartItems.jsx
@@ -1,9 +1,7 @@
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import Box from "@mui/material/Box";
 import { useCallback, useState } from "react";
-// import { Link } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-
 
 export default function RightCartItems() {
   const [state, setState] = useState(false);
@@ -20,9 +18,15 @@ export default function RightCartItems() {
     setState(type);
   }, []);
 
-  const openDrawerRightCart = useCallback(() => toggleDrawer(null, true), []);
+  const openDrawerRightCart = useCallback(
+    () => toggleDrawer(null, true),
+    [toggleDrawer]
+  );
 
-  const closeDrawerRightCart = useCallback(() => toggleDrawer(null, false), []);
+  const closeDrawerRightCart = useCallback(
+    () => toggleDrawer(null, false),
+    [toggleDrawer]
+  );
 
   const list = (
     <Box sx={{ width: 448 }} role="presentation">
@@ -52,20 +56,129 @@ export default function RightCartItems() {
             </svg>
           </button>
         </div>
-        {/* <div className="pt-20">
-          <ul className="grow">
-            {links.map((link, index) => (
-              <li key={index}>
-                <Link
-                  to={link.url}
-                  className="flex items-center px-5 py-3 transition duration-200 cursor-pointer LinkTextColorHover md:px-6"
-                >
-                  {link.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div> */}
+        <div className="pt-20">
+          <div className="border-b border-solid border-border-200 border-opacity-75 px-4 py-4 text-sm sm:px-6">
+            <div className="flex items-center justify-between">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex overflow-hidden flex-col-reverse items-center w-8 h-24 bg-gray-100 text-heading rounded-full">
+                    <button className="cursor-pointer p-2 transition-colors duration-200 hover:bg-accent-hover focus:outline-0 hover:!bg-gray-100">
+                      <svg
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="h-3 w-3 stroke-2.5"
+                      >
+                        <path d="M20 12H4"></path>
+                      </svg>
+                    </button>
+                    <div className="flex flex-1 items-center justify-center px-3 text-sm font-semibold text-heading">
+                      1
+                    </div>
+                    <button className="cursor-pointer p-2 transition-colors duration-200 hover:bg-accent-hover focus:outline-0 hover:!bg-gray-100">
+                      <span className="sr-only">plus</span>
+                      <svg
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="md:w-4.5 h-3.5 w-3.5 stroke-2.5 md:h-4.5"
+                      >
+                        <path d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div className="relative mx-4 flex h-10 w-10 shrink-0 items-center justify-center overflow-hidden bg-gray-100 sm:h-16 sm:w-16">
+                  <img
+                    alt="Apples"
+                    src="https://pickbazarlaravel.s3.ap-southeast-1.amazonaws.com/1/conversions/Apples-thumbnail.jpg"
+                    className="object-contain w-full h-full"
+                  />
+                </div>
+                <div>
+                  <h3 className="font-bold text-heading">Apples </h3>
+                  <p className="my-2.5 font-semibold text-accent">$1.60</p>
+                  <span className="text-xs text-body">1 X 1lb</span>
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <span className="font-bold text-heading ltr:ml-auto rtl:mr-auto">
+                  $0.60
+                </span>
+                <button className="flex h-7 w-7 shrink-0 items-center justify-center rounded-full text-muted transition-all duration-200 hover:bg-gray-100 hover:text-red-600 focus:bg-gray-100 focus:text-red-600 focus:outline-0">
+                  <svg
+                    className="h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="border-b border-solid border-border-200 border-opacity-75 px-4 py-4 text-sm sm:px-6">
+            <div className="flex items-center justify-between">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex overflow-hidden flex-col-reverse items-center w-8 h-24 bg-gray-100 text-heading rounded-full">
+                    <button className="cursor-pointer p-2 transition-colors duration-200 hover:bg-accent-hover focus:outline-0 hover:!bg-gray-100">
+                      <svg
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="h-3 w-3 stroke-2.5"
+                      >
+                        <path d="M20 12H4"></path>
+                      </svg>
+                    </button>
+                    <div className="flex flex-1 items-center justify-center px-3 text-sm font-semibold text-heading">
+                      1
+                    </div>
+                    <button className="cursor-pointer p-2 transition-colors duration-200 hover:bg-accent-hover focus:outline-0 hover:!bg-gray-100">
+                      <svg
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="md:w-4.5 h-3.5 w-3.5 stroke-2.5 md:h-4.5"
+                      >
+                        <path d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div className="relative mx-4 flex h-10 w-10 shrink-0 items-center justify-center overflow-hidden bg-gray-100 sm:h-16 sm:w-16">
+                  <img
+                    alt="Baby Spinach"
+                    src="https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F2%2FBabySpinach.jpg&w=1920&q=75"
+                    className="object-contain w-full h-full"
+                  />
+                </div>
+                <div>
+                  <h3 className="font-bold text-heading">Baby Spinach</h3>
+                  <p className="my-2.5 font-semibold text-accent">$0.60</p>
+                  <span className="text-xs text-body">1 X 2Pfund</span>
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <span className="font-bold text-heading ltr:ml-auto rtl:mr-auto">
+                  $0.60
+                </span>
+                <button className="flex h-7 w-7 shrink-0 items-center justify-center rounded-full text-muted transition-all duration-200 hover:bg-gray-100 hover:text-red-600 focus:bg-gray-100 focus:text-red-600 focus:outline-0">
+                  <svg
+                    className="h-3 w-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </Box>
   );
