9d6a05ee2fedc0dd7b665d1a2184afbf0d44d3d2
lalitjakhar <lalit25jakhar@gmail.com>
2024-07-01 12:11:45 +0530

    My wishlist ui

--- src/components/customerComponents/profile/my-wishlists/ItemsMyWishList.jsx
+++ src/components/customerComponents/profile/my-wishlists/ItemsMyWishList.jsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 import { IoIosStar } from "react-icons/io";
 import propTypes from "prop-types";
 import ProductsMyWishlists from "./ProductsMyWishlists";
+import { IoAdd } from "react-icons/io5";
+import { FiMinus } from "react-icons/fi";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import WishListRemoveToast from "../../../globalComponents/WishListRemoveToast";
 
 const wishListInfo = [
   {
@@ -11,6 +16,7 @@ const wishListInfo = [
       "https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F2%2FBabySpinach.jpg&w=1920&q=75",
     productName: "Baby Spinach",
     price: "$0.60",
+    rating: "3.33",
   },
   {
     shopName: "Grocery Shop",
@@ -18,6 +24,7 @@ const wishListInfo = [
       "https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F3%2Fblueberries.jpg&w=1920&q=75",
     productName: "Blueberries",
     price: "$3.00",
+    rating: "4.67",
   },
   {
     shopName: "Grocery Shop",
@@ -25,6 +32,7 @@ const wishListInfo = [
       "https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F1%2FApples.jpg&w=1920&q=75",
     productName: "Apples",
     price: "$1.60",
+    rating: "5",
   },
   {
     shopName: "Grocery Shop",
@@ -32,6 +40,7 @@ const wishListInfo = [
       "https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F2%2FBabySpinach.jpg&w=1920&q=75",
     productName: "Baby Spinach",
     price: "$0.60",
+    rating: "1.67",
   },
   {
     shopName: "Grocery Shop",
@@ -39,6 +48,7 @@ const wishListInfo = [
       "https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F3%2Fblueberries.jpg&w=1920&q=75",
     productName: "Blueberries",
     price: "$3.00",
+    rating: "2",
   },
   {
     shopName: "Grocery Shop",
@@ -46,6 +56,7 @@ const wishListInfo = [
       "https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F1%2FApples.jpg&w=1920&q=75",
     productName: "Apples",
     price: "$1.60",
+    rating: "5",
   },
   {
     shopName: "Grocery Shop",
@@ -53,6 +64,7 @@ const wishListInfo = [
       "https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F2%2FBabySpinach.jpg&w=1920&q=75",
     productName: "Baby Spinach",
     price: "$0.60",
+    rating: "3.33",
   },
   {
     shopName: "Grocery Shop",
@@ -60,6 +72,7 @@ const wishListInfo = [
       "https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F3%2Fblueberries.jpg&w=1920&q=75",
     productName: "Blueberries",
     price: "$3.00",
+    rating: "3.33",
   },
   {
     shopName: "Grocery Shop",
@@ -67,6 +80,7 @@ const wishListInfo = [
       "https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F1%2FApples.jpg&w=1920&q=75",
     productName: "Apples",
     price: "$1.60",
+    rating: "3.33",
   },
 ];
 
@@ -74,6 +88,7 @@ const ItemsMyWishList = ({ hideProductOfIndex = -1 }) => {
   const [counter, setCounter] = useState({});
   const [open, setOpen] = useState(false);
   const [productDetail, setProductDetail] = useState({});
+  const [wishlist, setWishlist] = useState(wishListInfo);
 
   const handleDetailOpen = (e, product, index) => {
     e.preventDefault();
@@ -87,21 +102,22 @@ const ItemsMyWishList = ({ hideProductOfIndex = -1 }) => {
   };
 
   const handleIncreaseCounter = (index) => {
-    let oldCounterObj = { ...counter };
-    if (!oldCounterObj[index]) {
-      oldCounterObj = {
-        ...oldCounterObj,
-        [index]: 0,
-      };
-    }
-    oldCounterObj[index] += 1;
-    setCounter(oldCounterObj);
+    setCounter((prevCounter) => ({
+      ...prevCounter,
+      [index]: (prevCounter[index] || 0) + 1,
+    }));
   };
 
   const handleDecreaseCounter = (index) => {
-    let oldCounterObj = { ...counter };
-    oldCounterObj[index] -= 1;
-    setCounter(oldCounterObj);
+    setCounter((prevCounter) => ({
+      ...prevCounter,
+      [index]: prevCounter[index] > 0 ? prevCounter[index] - 1 : 0,
+    }));
+  };
+
+  const handleRemove = (index) => {
+    setWishlist((prevWishlist) => prevWishlist.filter((_, i) => i !== index));
+    toast.success(<WishListRemoveToast />);
   };
   const style = {
     position: "relative",
@@ -110,9 +126,41 @@ const ItemsMyWishList = ({ hideProductOfIndex = -1 }) => {
       outline: "none !important",
     },
   };
+
+  const addToCartButton = (index) => (
+    <>
+      {counter[index] > 0 ? (
+        <div className="overflow-hidden w-7 h-18 sm:w-20 sm:h-7 md:h-9 md:w-24 rounded text-[#fff] bg-[#009f7f] inline-flex justify-between">
+          <button
+            className="cursor-pointer p-2 transition-colors duration-200 hover:bg-[#019376] focus:outline-0"
+            onClick={() => handleDecreaseCounter(index)}
+          >
+            <FiMinus />
+          </button>
+          <div className="flex flex-1 items-center justify-center px-3 text-sm font-semibold">
+            {counter[index]}
+          </div>
+          <button
+            className="cursor-pointer p-2 transition-colors duration-200 hover:bg-[#019376] focus:outline-0"
+            onClick={() => handleIncreaseCounter(index)}
+          >
+            <IoAdd />
+          </button>
+        </div>
+      ) : (
+        <button
+          onClick={() => handleIncreaseCounter(index)}
+          className="whitespace-nowrap text-sm font-semibold text-[#009f7f] hover:text-[#019376]-hover hover:underline"
+        >
+          Add to Cart
+        </button>
+      )}
+    </>
+  );
+
   return (
     <div>
-      {wishListInfo.map((product, index) => {
+      {wishlist.map((product, index) => {
         if (hideProductOfIndex === index) return null;
         return (
           <div
@@ -126,7 +174,6 @@ const ItemsMyWishList = ({ hideProductOfIndex = -1 }) => {
               <div className="flex w-full flex-col sm:items-start">
                 <a
                   className="text-lg font-semibold text-[#1f2937] transition-colors hover:text-[#019376]"
-                  // href="/"
                   onClick={(e) => handleDetailOpen(e, product, index)}
                 >
                   {product.productName}
@@ -139,9 +186,9 @@ const ItemsMyWishList = ({ hideProductOfIndex = -1 }) => {
                   >
                     {product.shopName}
                   </a>
-                  <span className="inline-flex shrink-0 items-center bg-[#009f7f] text-white px-2 py-[3px] text-sm !rounded">
-                    3.33
-                    <span className="flex h-2.5 w-2.5 ltr:ml-1 rtl:mr-1">
+                  <span className="flex gap-1 shrink-0 items-center bg-[#009f7f] text-white px-2 py-[3px] text-[14px] !rounded">
+                    {product.rating}
+                    <span className="flex h-[16px] w-[16px]">
                       <IoIosStar />
                     </span>
                   </span>
@@ -154,13 +201,12 @@ const ItemsMyWishList = ({ hideProductOfIndex = -1 }) => {
                   </ins>
                 </span>
                 <div className="flex items-center space-x-6 rtl:space-x-reverse sm:justify-end">
-                  <div>
-                    <button className="whitespace-nowrap text-sm font-semibold text-[#009f7f] hover:text-[#019376]-hover hover:underline">
-                      Add to Cart
-                    </button>
-                  </div>
+                  <div>{addToCartButton(product, index)}</div>
                   <span className="flex h-7 w-px border-r border-dashed border-gray-300"></span>
-                  <button className="whitespace-nowrap text-sm font-semibold text-red-500 hover:underline sm:mt-0">
+                  <button
+                    className="whitespace-nowrap text-sm font-semibold text-red-500 hover:underline sm:mt-0"
+                    onClick={() => handleRemove(index)}
+                  >
                     Remove
                   </button>
                 </div>
@@ -201,6 +247,7 @@ const ItemsMyWishList = ({ hideProductOfIndex = -1 }) => {
 };
 
 export default ItemsMyWishList;
+
 ItemsMyWishList.propTypes = {
   hideProductOfIndex: propTypes.number,
 };
